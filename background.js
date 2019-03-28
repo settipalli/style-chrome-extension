85bc7cf6359d8f680282ebc50158ea2445c1c921
Santhoshkumar Settipalli <santhosh.settipalli@gmail.com>
2019-03-28 14:48:03 +0000

    Updated the font list. Removed the import statement. Added the functionality to enable/disable the extension icon.

--- background.js
+++ background.js
@@ -3,9 +3,19 @@ chrome.browserAction.onClicked.addListener(function (tab) {
     // send a message to the active tab
     chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
         var activeTab = tabs[0];
-        chrome.tabs.sendMessage(activeTab.id, {
-            "message": "clicked_browser_action_event",
-            "url": chrome.extension.getURL("styles.css")
-        })
+        chrome.tabs.sendMessage(
+            activeTab.id,
+            {
+                "message": "clicked_browser_action_event",
+                "url": chrome.extension.getURL("styles.css")
+            },
+            function(response) {
+                if (response.status == "show_enabled_icon") {
+                    chrome.browserAction.setIcon({path: "icons/icon_128.png"});
+                } else {
+                    chrome.browserAction.setIcon({path: "icons/icon_128_disabled.png"});
+                }
+            }
+        );
     });
 });
--- content.js
+++ content.js
@@ -3,15 +3,17 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         let id = "stylescss";
         if (request.message === "clicked_browser_action_event") {
             // if styles.css is active, toggle it.
-            var element = document.getElementById(id);
+            let element = document.getElementById(id);
             if (element) {
                 element.parentNode.removeChild(element);
+                sendResponse({status: "show_disabled_icon"});
             } else {
                 let link = document.createElement("link");
                 link.rel = "stylesheet";
                 link.href = request.url;
                 link.id = id;
                 document.getElementsByTagName("head")[0].appendChild(link);
+                sendResponse({status: "show_enabled_icon"});
             }
         }
 });
